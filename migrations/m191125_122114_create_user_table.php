dbfb67aa6fcb97cc5bd9730cd779c4938efc40eb
asilb <asilbekmubarak@gmail.com>
2019-12-20 09:49:26 +0600

    AW-1 fix: rename varchar into string

--- migrations/m191125_122114_create_user_table.php
+++ migrations/m191125_122114_create_user_table.php
@@ -14,8 +14,8 @@ class m191125_122114_create_user_table extends Migration
     {
         $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
-            'username' => $this->char(255)->notNull()->unique(),
-            'password'=>$this ->varchar(255)->notNull(),
+            'username' => $this->string(255)->notNull()->unique(),
+            'password'=>$this->char(255)->notNull(),
             'role' =>$this->char(255)->notNull()->defaultValue('user'),
             'mail'=>$this->char(255)->notNull()->unique()
         ]);
